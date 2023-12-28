fce7f7e5b73306deb7f9d6617d121aa7ce7461dd
Janarbek <janarbek.bekmuyrzauulu.alt@gmail.com>
2023-12-28 11:25:12 +0600

    fix

--- src/pages/accessories-page/accessories-page.jsx
+++ src/pages/accessories-page/accessories-page.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function AccessoriesPage() {
   return (
     <div>
-      <div className="mx-auto lg:w-[1170px]">AccessoriesPage</div>
+      <div className="mx-auto lg:w-[1170px]">
+        <h1>AccessoriesPage</h1>
+      </div>
     </div>
   );
 }
